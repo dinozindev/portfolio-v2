4744a29c2d090679fd3a223ea4f5af2c8609f0bb
dinozindev <dinozindev@gmail.com>
2026-02-27 14:51:45 -0300

    feat: ajustando a estilização dos mini widgets e adicionando projeto Smart Energy

--- src/Components/ProjectMiniWidget.tsx
+++ src/Components/ProjectMiniWidget.tsx
@@ -10,10 +10,10 @@ type MiniWidgetProps = {
 
 const ProjectMiniWidget = ({ title, description, skills, linkGithub, linkWebsite, linkYoutube }: MiniWidgetProps) => {
     return (
-        <div className="bg-blue-950 w-1/4 px-6 py-9 flex flex-col justify-between">
+        <div className="bg-miniwidget w-1/4 px-6 py-9 flex flex-col justify-between shadow-2xl hover:translate-z-2.5 hover:-translate-y-2.5 transition-all">
             <h3 className="text-3xl mb-3">{title}</h3>
             <p className="mb-3">{description}</p>
-            <div className="flex flex-wrap gap-3">
+            <div className="flex flex-wrap gap-3 mb-3">
             {skills.map((skill, i) => (
                 <p className="bg-orange-300 text-black px-1.5 rounded-sm text-sm" key={i}>{skill}</p>
             ))}
