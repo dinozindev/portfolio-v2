0116437f55803f0922d161190e3b653653bce60d
dinozindev <dinozindev@gmail.com>
2026-03-03 13:17:32 -0300

    refactor: reajustando padding do header de section

--- src/Components/SectionHeader.tsx
+++ src/Components/SectionHeader.tsx
@@ -7,7 +7,7 @@ type SectionHeaderProps = {
 const SectionHeader = ({number, text}: SectionHeaderProps) => {
   return (
     <>
-    <header className="flex mb-8 items-center md:justify-start gap-6">
+    <header className="flex mb-8 mt-16 items-center md:justify-start gap-6">
         <div className="flex gap-2 items-end">
         <p className="text-orange-300 text-3xl">{number}.</p>
         <h3 className="text-4xl">{text}</h3>
